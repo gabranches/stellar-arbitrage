fc84da657b67d496b073ab67edc55cb2183719e6
unknown <gabranches@gmail.com>
2019-03-18 15:12:03 -0400

    minor

--- index.js
+++ index.js
@@ -3,8 +3,17 @@ const Arbitrage = require('./src/Arbitrage')
 const Bittrex = require('./api/Bittrex')
 const StellarDEX = require('./api/StellarDEX')
 
+
+const timer = time => {
+  return new Promise((resolve, reject) => {
+    console.log(`\nWaiting ${time}ms...\n`)
+    setTimeout(() => resolve(), time)
+  })
+}
+
 run = async () => {
   try {
+    await timer(10000)
     await new Arbitrage()
       .asset('MOBI')
       .addMarket('bittrex-BTC')
@@ -14,14 +23,7 @@ run = async () => {
   } catch (error) {
     console.error(error)
   }
-  await timer(10000)
   run()
 }
 run()
 
-const timer = time => {
-  return new Promise((resolve, reject) => {
-    console.log(`Waiting ${time}ms...`)
-    setTimeout(() => resolve(), time)
-  })
-}
